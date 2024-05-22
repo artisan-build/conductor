3457082d7aca12abe6b0f86782801347a875eb44
Len Woodward <Len@ProjektGopher.com>
2024-05-22 15:21:52 -0700

    wip

--- app/Commands/RunCommand.php
+++ app/Commands/RunCommand.php
@@ -3,6 +3,7 @@
 namespace Conductor\Commands;
 
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Process;
 use LaravelZero\Framework\Commands\Command;
 use NunoMaduro\LaravelConsoleSummary\SummaryCommand;
 
@@ -22,20 +23,41 @@ class RunCommand extends Command
      */
     protected $description = 'Install and run a composer package.';
 
+    protected string $package;
+
     /**
      * Execute the console command.
      */
     public function handle()
     {
+        // check if conductor is latest version...
+
         if (! $this->arguments()['package']) {
             $this->call(SummaryCommand::class);
+
             return;
         }
 
-        $this->info('checking package for binary...');
-        $this->getBinaryName();
+        $this->package = $this->arguments()['package'];
 
-        $this->info('checking for globally installed package...');
+        $binary = $this->getBinaryName();
+
+        if ($this->packageIsInstalledLocally()) {
+            $this->info('package is already installed locally. running...');
+            $cmd = "vendor/bin/{$binary} {$this->arguments()['thing']} {$this->arguments()['argument']}";
+            $result = Process::forever()->tty()->run($cmd);
+
+            return $result->exitCode();
+        }
+
+        if ($this->packageIsInstalledGlobally()) {
+            $this->info('package is already installed globally. running...');
+            $cmd = "{$binary} {$this->arguments()['thing']} {$this->arguments()['argument']}";
+            $result = Process::forever()->tty()->run($cmd);
+
+            return $result->exitCode();
+        }
+
         $this->info('update globally installed package?..');
         $this->info('keep package installed after running?..');
         $this->info('installing package globally...');
@@ -44,14 +66,32 @@ class RunCommand extends Command
         $this->info('Command ran successfully!');
     }
 
-    protected function getBinaryName()
+    protected function getBinaryName(): string
     {
-        $package = $this->arguments()['package'];
-        $response = Http::get("https://packagist.org/packages/{$package}.json");
+        $this->info('checking package for binary...');
+        $response = Http::get("https://packagist.org/packages/{$this->package}.json");
         $json = $response->json();
         $versions = $json['package']['versions'];
         $latest = $versions[array_keys($versions)[1]]; // [0] is `dev-master` or `dev-main`
         $binary = $latest['bin'][0];
-        $this->info('Binary name: ' . $binary);
+        // $this->info('Binary name: ' . $binary);
+
+        return explode('/', $binary)[1];
+    }
+
+    protected function packageIsInstalledLocally(): bool
+    {
+        $this->info('checking for locally installed package...');
+        exec("composer show {$this->arguments()['package']} 2>&1", $output, $return_code);
+
+        return $return_code === 0;
+    }
+
+    protected function packageIsInstalledGlobally(): bool
+    {
+        $this->info('checking for globally installed package...');
+        exec("composer global show {$this->arguments()['package']} 2>&1", $output, $return_code);
+
+        return $return_code === 0;
     }
 }
